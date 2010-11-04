f2897b5f70fea92ce150e836ca50bee27cc82151
hemanth <hemanth.hm@gmail.com>
2010-11-04 19:09:36 +0530

    Added goog i'm feeling lucky method

--- rgbot.rb
+++ rgbot.rb
@@ -4,19 +4,18 @@
 
 class L33ty
 
-
     def initialize(bot_name,bot_password)
         @bot  =  bot_name
         @pass = bot_password
         @jabber = Jabber::Simple.new(@bot+'@gmail.com',@pass)
     end
-    
-    def invoke(msg)
+
+        def invoke(msg)
         restricted_methods=['main','invoke','deliver']
         meth=msg.body.split()[0]
         self.send(meth,msg) if self.respond_to?(meth) and !restricted_methods.include?(meth) 
     end
-    
+
     def deliver(msg,res)
         @jabber.deliver(msg.from.node+"@gmail.com",res)
     end
@@ -26,11 +25,10 @@ class L33ty
     end
     
     def goog(msg)
-        
-        
-        
+         gurl = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q="+URI.escape(msg.body, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+         self.deliver(msg,JSON.parse(Net::HTTP.get_response(URI.parse(gurl)).body)['responseData']['results'][0]['url'])  
     end
-    
+
     def main   
         while (true) do  
              @jabber.received_messages do |msg|  
